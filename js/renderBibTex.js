b06cad02b5bb53ddfd05a70291d23dd210161085
ControlNet <smczx@hotmail.com>
2021-11-02 09:55:20 +1100

    update the counter text

--- js/renderBibTex.js
+++ js/renderBibTex.js
@@ -3,11 +3,14 @@
         const fullAbsRe = /\tabstract = {.+?},/;
         data = data.split("\n").filter(row => !row.match(fullAbsRe)).join("\n")
         $("#bibtex_input").val(data);
-        console.log(`My Library.bib Loaded: ${data.length}`)
+        console.log(`My Library.bib Loaded: ${data.length}`);
     }
 
     $.get('/data/My Library.bib', renderBibTex).then(() => {
         $.getScript("https://cdn.jsdelivr.net/gh/pcooksey/bibtex-js@1.0.0/src/bibtex_js.js");
+        $("body").on("DOMSubtreeModified", "#bibtex_display", () => {
+            $("p#counter").html(`No. BibTex Loaded: ${document.getElementsByClassName("bibtexentry").length}`);
+        });
     });
 
 })(jQuery);
